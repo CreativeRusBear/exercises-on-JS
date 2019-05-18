0cda05723fc54ca547ea60bf2ee9b7df1027ea4c
Artem <gusev2014russia@mail.ru>
2019-05-18 12:32:54 +0300

    add challenge from google interview

---
+++ challenge from google interview/first.js
@@ -0,0 +1,11 @@
+//Решение 1. Брутфорс
+const findSum=(arr, val)=>{
+    for (let i=0; i<arr.length; i++){
+        for (let j=0; j<arr.length; j++){
+            if (i!==j && arr[i]+arr[j]===val) return true;
+        };
+    };
+    return false;
+};
+
+console.log(findSum([1,2,3,4],5));
